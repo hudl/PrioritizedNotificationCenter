40be0b3b825ebe8de96e1f6c497655865cd081eb
Juanjo Ramos <jjramos.developer@gmail.com>
2015-10-01 14:38:48 +0100

    Running pod install to incorporate the latest changes in the pod

--- Example/Pods/Target Support Files/PRNotificationCenter/PRNotificationCenter-umbrella.h
+++ Example/Pods/Target Support Files/PRNotificationCenter/PRNotificationCenter-umbrella.h
@@ -1,5 +1,6 @@
 #import <UIKit/UIKit.h>
 
+#import "PRNotificationCenter.h"
 
 FOUNDATION_EXPORT double PRNotificationCenterVersionNumber;
 FOUNDATION_EXPORT const unsigned char PRNotificationCenterVersionString[];
